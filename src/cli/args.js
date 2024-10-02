192d139af36f92af699561dde87a29c8cab08d6b
Yakubenka Ivan <palasja@gmail.com>
2024-10-03 01:09:01 +0300

    feat: solve cli task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+//process.argv[0] is process.execPath
+//process.argv[1] is the path to the JavaScript file
+//start from 3 element with step=2 because key-valuse pair
+const _STEP = 2;
+const _PREFIX = '--';
+for(let i=2; i < process.argv.length; i+=_STEP){
+    console.log(`${process.argv[i].slice(_PREFIX.length)} is ${process.argv[i+1]}`);
+}
     
 };
 
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,8 @@
 const parseEnv = () => {
-    // Write your code here 
+    const reg = /^RSS_/;
+    for(const key in process.env){
+        if(reg.test(key)) console.log(`${key}=${process.env[key]};`);
+    }
 };
 
 parseEnv();

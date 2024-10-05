e84041431e8cc5cf1124db9415e5da774b580835
Yakubenka Ivan <palasja@gmail.com>
2024-10-05 03:25:56 +0300

    refactor: remove coment line

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,27 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { pipeline } from 'node:stream/promises';
+import { createGzip } from 'node:zlib';
+
 const compress = async () => {
-    // Write your code here 
+    const _FOLDER_NAME = 'files';
+    const _FILE_NAME = 'fileToCompress.txt';
+    const _ZIP_FILE_NAME = 'archive.gz';
+
+    const cur_path = dirname(import.meta.url);
+    const path = join(cur_path, _FOLDER_NAME, _FILE_NAME);
+    const urlPath = fileURLToPath(path);
+
+    const zipPath = join(cur_path, _FOLDER_NAME, _ZIP_FILE_NAME);
+    const zipUrlPath = fileURLToPath(zipPath);
+
+    const gzip = createGzip();
+    await pipeline(
+        createReadStream(urlPath),
+        gzip,
+        createWriteStream(zipUrlPath),
+    );
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,35 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { access, rm} from 'node:fs/promises';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { pipeline } from 'node:stream/promises';
+import { createGunzip } from 'node:zlib';
+
 const decompress = async () => {
-    // Write your code here 
+    const _FOLDER_NAME = 'files';
+    const _FILE_NAME = 'fileToCompress.txt';
+    const _ZIP_FILE_NAME = 'archive.gz';
+
+    const cur_path = dirname(import.meta.url);
+    const path = join(cur_path, _FOLDER_NAME, _FILE_NAME);
+    const urlPath = fileURLToPath(path);
+
+    const zipPath = join(cur_path, _FOLDER_NAME, _ZIP_FILE_NAME);
+    const zipUrlPath = fileURLToPath(zipPath);
+
+    await access(urlPath).then(
+        //if file exist remove it
+        async () => {
+            await rm(urlPath);
+        }
+    )
+
+    const gzip = createGunzip();
+    await pipeline(
+        createReadStream(zipUrlPath),
+        gzip,
+        createWriteStream(urlPath),
+    );
 };
 
 await decompress();
